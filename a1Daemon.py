3bcad944091bb8921d512d60f7e54e59ca7b1a4b
Sang Min Park <9714park@gmail.com>
2019-11-07 09:52:40 -0500

    added DaemonServer class

--- a1Daemon.py
+++ a1Daemon.py
@@ -3,6 +3,12 @@
 # DPI 912 Daemon
 
 import socket
+import logzero
+from logzero import logger
+import os
+import signal
+import sys
+import atexit
 
 
 class Server(object):
@@ -51,6 +57,81 @@ class Server(object):
             self.serverSocket.close()
         except Exception as err:
             raise Exception(err)
+
+
+class DaemonServer(Server):
+    def __init__(self, pidfile, ipAddress, socketNumber, queueSize=1024):
+        self.pidfile = pidfile
+        Server.__init__(self, ipAddress, socket, queueSize)
+
+    def daemonize(self, pidfile, *, stdin='/dev/null',
+                  stdout='/dev/null',
+                  stderr='/dev/null'):
+        """The code below is adapted by:
+        https://github.com/dabeaz/python-cookbook/blob/master/
+        src/12/launching_a_daemon_process_on_unix/daemon.py
+        It uses Unix double-fork magic based on Stevens's book
+        "Advanced Programming in the UNIX Environment".
+        Creates a daemon that is diassociated with the terminal
+        and has no root privileges. Once double-forking is successful
+        it writes its pid to a designated pidfile. The pidfile
+        is later used to kill the daemon. """
+
+        # If pidfile exists, there is a server program that is currently running
+        if os.path.exists(self.pidfile):
+            raise RuntimeError('Already running')
+
+        # First fork (detaches from parent)
+        try:
+            if os.fork() > 0:
+                # Parent exit
+                raise SystemExit(0)
+        except OSError as e:
+            raise RuntimeError(f'fork #1 failed: {e}')
+
+        # Decouple from parent environment
+        os.chdir('/tmp')
+        os.umask(0)
+        os.setsid()
+        dropPrivileges()
+
+        logger.info("fork#1 successfull")
+        # Second fork (relinquish session leadership)
+        try:
+            if os.fork() > 0:
+                raise SystemExit(0)
+        except OSError as e:
+            raise RuntimeError(f'fork #2 failed: {e}')
+
+        # Flush I/O buffers
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        # Replace file descriptors for stdin, stdout, and stderr
+        with open(stdin, 'rb', 0) as f:
+            os.dup2(f.fileno(), sys.stdin.fileno())
+        with open(stdout, 'ab', 0) as f:
+            os.dup2(f.fileno(), sys.stdout.fileno())
+        with open(stderr, 'ab', 0) as f:
+            os.dup2(f.fileno(), sys.stderr.fileno())
+
+        # PID of the double-forked daemon
+        fork2DaemonPID = os.getpid()
+
+        # Write the PID file
+        with open(self.pidfile, 'w') as f:
+            print(fork2DaemonPID, file=f)
+
+        logger.info(f"fork#2 successful pid[{fork2DaemonPID}]")
+
+        # Arrange to have the PID file removed on exit/signal
+        atexit.register(lambda: os.remove(pidfile))
+
+        # Signal handler for termination (required)
+        def sigterm_handler(signo, frame):
+            raise SystemExit(1)
+
+        signal.signal(signal.SIGTERM, sigterm_handler)
 
 
 if __name__ == '__main__':
