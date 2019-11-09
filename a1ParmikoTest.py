7b85c1acb020ca31b721949aaea3244b08122748
Jacky Tea <teajacks@outlook.com>
2019-11-09 18:43:42 -0500

    paramiko test file

---
+++ a1ParmikoTest.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+
+import paramiko
+import socket
+
+class sshClient(object):
+    #constructor, sets necessary parameters to establish a SSH tunnel
+    def __init__(self,  hostName,  portNumber, userName,  passWord):
+        self.SSHClient = None
+        self.hostName = hostName
+        self.portNumber = portNumber
+        self.userName = userName
+        self.passWord = passWord
+    
+    #make a connection to a remote SSH server
+    def makeSSHConnection(self):
+        try:
+            #create a client instance, auto generate a key 
+            #and attempt to connect to a host
+            self.SSHClient = paramiko.SSHClient()
+            self.SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.SSHClient.connect(hostname=self.hostName,  port=self.portNumber, \
+            username=self.userName,  password=self.passWord)
+            print('SSH Connection Established . . .')
+            #handle errors
+        except paramiko.AuthenticationException as authErr:
+            print('Authentication failed:',  authErr)
+        except paramiko.SSHException as sshErr:
+            print('SSH connection failed:',  sshErr)
+        except socket.timeout as toutErr:
+            print('Connection timed out:',  toutErr)
+        except Exception as err:
+            print('Exception has ocurred:',  err)
+            self.SSHClient.close()
+    
+    #executes a command on a remote host
+    def executeCommand(self,  command):
+        try:
+            #connect to SSH server
+            self.makeSSHConnection()
+            #run the exec_command() method to 
+            #execute a command remotely
+            stdin,  stdout,  stderr = self.SSHClient.exec_command(command, \
+            timeout=10)
+            print(str(stdout.read()))
+            if stderr:
+                print(str(stderr.read()))
+            self.SSHClient.close()
+            #handle errors
+        except paramiko.SSHException as sshErr:
+            print('Command execution failed:',  sshErr)
+            self.SSHClient.close()
+        except socket.timeout as toutErr:
+            print('Command timed out:',  toutErr)
+            self.SSHClient.close()
+        except Exception as err:
+            print('Exception has ocurred:',  err)
+            self.SSHClient.close()
+    
+    #upload a file via the SSH tunnel to remote server via SFTP
+    def uploadFile(self,  localPath,  remotePath):
+        try:
+            #connect to SSH server
+            self.makeSSHConnection()
+            #create an SFTP tunnel
+            SFTPInstance = self.SSHClient.open_sftp()
+            SFTPInstance.put(localPath,  remotePath)
+            #handle errors
+        except Exception as err:  
+            print('Exception has ocurred:',  err)
+            SFTPInstance.close()
+            self.SSHClient.close()
+            
+    #download a file via the SSH tunnel from remote server via SFTP
+    def downloadFile(self,  localPath,  remotePath):
+        try:
+            #connect to SSH server
+            self.makeSSHConnection()
+            #create an SFTP tunnel
+            SFTPInstance = self.SSHClient.open_sftp()
+            SFTPInstance.get(localPath,  remotePath)
+            #handle errors
+        except Exception as err:  
+            print('Exception has ocurred:',  err)
+            SFTPInstance.close()
+            self.SSHClient.close()
+    
+#main thread
+if __name__ == '__main__':
+    #sshClient = sshClient(host, port, user, pass)
+    #sshClient.executeCommand('touch testSSH')
+    pass
+
+        
+    
