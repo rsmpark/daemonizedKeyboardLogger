355e22a26ab6afc520a687001e5a0604ea8ce7e2
Sang Min Park <9714park@gmail.com>
2019-11-12 09:32:01 -0500

    modifide print() statements in executeCommand()

--- a1ParmikoTest.py
+++ a1ParmikoTest.py
@@ -3,26 +3,29 @@
 import paramiko
 import socket
 
+
 class sshClient(object):
-    #constructor, sets necessary parameters to establish a SSH tunnel
+    # constructor, sets necessary parameters to establish a SSH tunnel
     def __init__(self,  hostName,  portNumber, userName,  passWord):
         self.SSHClient = None
         self.hostName = hostName
         self.portNumber = portNumber
         self.userName = userName
         self.passWord = passWord
-    
-    #make a connection to a remote SSH server
+
+    # make a connection to a remote SSH server
     def makeSSHConnection(self):
         try:
-            #create a client instance, auto generate a key 
-            #and attempt to connect to a host
+            print("Establishing ssh connection")
+            # create a client instance, auto generate a key
+            # and attempt to connect to a host
             self.SSHClient = paramiko.SSHClient()
-            self.SSHClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.SSHClient.connect(hostname=self.hostName,  port=self.portNumber, \
-            username=self.userName,  password=self.passWord)
+            self.SSHClient.set_missing_host_key_policy(
+                paramiko.AutoAddPolicy())
+            self.SSHClient.connect(hostname=self.hostName,  port=self.portNumber,
+                                   username=self.userName,  password=self.passWord)
             print('SSH Connection Established . . .')
-            #handle errors
+            # handle errors
         except paramiko.AuthenticationException as authErr:
             print('Authentication failed:',  authErr)
         except paramiko.SSHException as sshErr:
@@ -32,21 +35,21 @@ class sshClient(object):
         except Exception as err:
             print('Exception has ocurred:',  err)
             self.SSHClient.close()
-    
-    #executes a command on a remote host
+
+    # executes a command on a remote host
     def executeCommand(self,  command):
         try:
-            #connect to SSH server
+            # connect to SSH server
             self.makeSSHConnection()
-            #run the exec_command() method to 
-            #execute a command remotely
-            stdin,  stdout,  stderr = self.SSHClient.exec_command(command, \
-            timeout=10)
-            print(str(stdout.read()))
+            # run the exec_command() method to
+            # execute a command remotely
+            stdin,  stdout,  stderr = self.SSHClient.exec_command(command,
+                                                                  timeout=10)
+            print(f"command STDOUT: {str(stdout.read())}")
             if stderr:
-                print(str(stderr.read()))
+                print(f"command STDERR: {str(stderr.read())}")
             self.SSHClient.close()
-            #handle errors
+            # handle errors
         except paramiko.SSHException as sshErr:
             print('Command execution failed:',  sshErr)
             self.SSHClient.close()
@@ -56,40 +59,38 @@ class sshClient(object):
         except Exception as err:
             print('Exception has ocurred:',  err)
             self.SSHClient.close()
-    
-    #upload a file via the SSH tunnel to remote server via SFTP
+
+    # upload a file via the SSH tunnel to remote server via SFTP
     def uploadFile(self,  localPath,  remotePath):
         try:
-            #connect to SSH server
+            # connect to SSH server
             self.makeSSHConnection()
-            #create an SFTP tunnel
+            # create an SFTP tunnel
             SFTPInstance = self.SSHClient.open_sftp()
             SFTPInstance.put(localPath,  remotePath)
-            #handle errors
-        except Exception as err:  
+            # handle errors
+        except Exception as err:
             print('Exception has ocurred:',  err)
             SFTPInstance.close()
             self.SSHClient.close()
-            
-    #download a file via the SSH tunnel from remote server via SFTP
+
+    # download a file via the SSH tunnel from remote server via SFTP
     def downloadFile(self,  localPath,  remotePath):
         try:
-            #connect to SSH server
+            # connect to SSH server
             self.makeSSHConnection()
-            #create an SFTP tunnel
+            # create an SFTP tunnel
             SFTPInstance = self.SSHClient.open_sftp()
             SFTPInstance.get(localPath,  remotePath)
-            #handle errors
-        except Exception as err:  
+            # handle errors
+        except Exception as err:
             print('Exception has ocurred:',  err)
             SFTPInstance.close()
             self.SSHClient.close()
-    
-#main thread
-if __name__ == '__main__':
-    #sshClient = sshClient(host, port, user, pass)
-    #sshClient.executeCommand('touch testSSH')
-    pass
 
-        
-    
+
+# main thread
+if __name__ == '__main__':
+    sshClient = sshClient("debian10vm", 22, "lab", "lab")
+    sshClient.executeCommand('touch testSSH')
+    pass
